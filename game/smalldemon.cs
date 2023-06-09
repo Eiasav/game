785d62da0c30b81634f9af3ce51e679ef17c9819
Eiasav <selyanina.vika.vs@gmail.com>
2023-06-09 14:06:38 +0500

    final(

--- game/smalldemon.cs
+++ game/smalldemon.cs
@@ -3,19 +3,47 @@ using System;
 
 public class smalldemon : KinematicBody2D
 {
-    // Declare member variables here. Examples:
-    // private int a = 2;
-    // private string b = "text";
+    Sprite sprite;
+    RayCast2D rayCastLeft;
+    RayCast2D rayCastRight;
+    private Vector2 velocity;
+    private int speed = 30;
+    private AnimationPlayer player;
 
-    // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
-        
+        sprite = GetNode<Sprite>("Sprite");
+        rayCastLeft = GetNode<RayCast2D>("leftRayCast");
+        rayCastRight = GetNode<RayCast2D>("rightRayCast");
+        player = GetNode<AnimationPlayer>("AnimationPlayer");
+        velocity.x = speed;
     }
 
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
+    public override void _PhysicsProcess(float delta)
+    {
+        if(rayCastRight.IsColliding())
+        {
+            velocity.x = -speed;
+            sprite.FlipH = false;
+        }
+        else if (rayCastLeft.IsColliding())
+        {
+            velocity.x = speed;
+            sprite.FlipH = true;
+        }
+        if (!player.IsPlaying())
+        {
+            player.Play("fly");
+        }
+        MoveAndSlide(velocity, Vector2.Up);
+    }
+
+    public void _on_Area2D_body_entered(object body)
+    {
+        if (body is player_movement)
+        {
+            player_movement player = body as player_movement;
+            player.TakeDamage();
+        }
+    }
 }
